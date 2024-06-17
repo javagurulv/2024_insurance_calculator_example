0a55605362ee2da5a58d7e6fe720bea634fdcccd
vladkuzhyr <vkuzir7@gmail.com>
2024-06-17 17:08:57 +0300

    STEP 14 - DONE

--- student_vladislav_kuzir/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_vladislav_kuzir/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -3,30 +3,46 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
 class TravelCalculatePremiumServiceImplTest {
 
-  private TravelCalculatePremiumService travelCalculatePremiumService = new TravelCalculatePremiumServiceImpl();
+  private static TravelCalculatePremiumResponse travelCalculatePremiumResponse;
 
-  @Test
-  public void responseTest() {
+  @BeforeAll
+  static void setUp() {
+    TravelCalculatePremiumService travelCalculatePremiumService =
+            new TravelCalculatePremiumServiceImpl();
     TravelCalculatePremiumRequest travelCalculatePremiumRequest = new TravelCalculatePremiumRequest();
     travelCalculatePremiumRequest.setPersonFirstName("IsmailYue");
     travelCalculatePremiumRequest.setPersonLastName("Mohammad");
     travelCalculatePremiumRequest.setAgreementDateFrom(new Date());
     travelCalculatePremiumRequest.setAgreementDateTo(new Date());
+    travelCalculatePremiumResponse = travelCalculatePremiumService.calculatePremium(travelCalculatePremiumRequest);
 
-    TravelCalculatePremiumResponse travelCalculatePremiumResponse = travelCalculatePremiumService
-            .calculatePremium(travelCalculatePremiumRequest);
-
-    Assertions.assertEquals(travelCalculatePremiumResponse.getPersonFirstName(), "IsmailYue");
-    Assertions.assertEquals(travelCalculatePremiumResponse.getPersonLastName(), "Mohammad");
-    Assertions.assertNotNull(travelCalculatePremiumResponse.getAgreementDateFrom());
-    Assertions.assertNotNull(travelCalculatePremiumResponse.getAgreementDateTo());
   }
 
+  @Test
+  void getFirstNameFromResponseTest() {
+    Assertions.assertEquals(travelCalculatePremiumResponse.getPersonFirstName(), "IsmailYue");
+  }
+
+  @Test
+  void getLastNameFromResponseTest() {
+    Assertions.assertEquals(travelCalculatePremiumResponse.getPersonLastName(), "Mohammad");
+  }
+
+  @Test
+  void getAgreementDateFromResponseTest() {
+    Assertions.assertNotNull(travelCalculatePremiumResponse.getAgreementDateFrom());
+  }
+
+  @Test
+  void getAgreementDateToResponseTest() {
+    Assertions.assertNotNull(travelCalculatePremiumResponse.getAgreementDateTo());
+  }
 
 }
