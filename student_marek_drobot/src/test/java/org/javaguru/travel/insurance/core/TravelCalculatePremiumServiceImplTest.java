303784909e294ae1b85c2b50d723822329f3184b
Marek Drobot <ch4ospark@gmail.com>
2024-06-19 11:41:52 +0300

    STEP 014 - DONE

--- student_marek_drobot/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_marek_drobot/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -10,20 +10,55 @@ import java.util.Date;
 class TravelCalculatePremiumServiceImplTest {
 
     private TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+    private TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
 
     @Test
-    public void responseTest(){
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+    public void responseFirstNameTest(){
+        // заполняем поле имени
         request.setPersonFirstName("Marina");
-        request.setPersonLastName("Seliv");
-        request.setAgreementDateFrom(new Date());
-        request.setAgreementDateTo(new Date());
 
+        // получаем ответ на основе нашего запроса
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
 
+        // проверяем правильность заполнения
         Assertions.assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
+
+    }
+
+    @Test
+    public void responseLastNameTest(){
+        // дополняем запрос значением фамилии
+        request.setPersonLastName("Seliv");
+
+        // получаем ответ на основе нашего запроса
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        // проверяем правильность заполнения
         Assertions.assertEquals(response.getPersonLastName(), request.getPersonLastName());
+
+    }
+
+    @Test
+    public void responseAgreementDateFromTest(){
+        // дополняем запрос значением даты начала
+        request.setAgreementDateFrom(new Date());
+
+        // получаем ответ на основе нашего запроса
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        // проверяем правильность заполнения
         Assertions.assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
+    }
+
+    @Test
+    public void responseAgreementDateToTest(){
+        // дополняем запрос значением даты конца
+        request.setAgreementDateTo(new Date());
+
+        // получаем ответ на основе нашего запроса
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        // проверяем правильность заполнения
         Assertions.assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
 
