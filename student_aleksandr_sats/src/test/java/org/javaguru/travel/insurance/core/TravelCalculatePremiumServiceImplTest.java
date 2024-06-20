f3b5a213450aa394d193fc4a9641d0b52d8faa67
Aleksandr Sats <aleksandr.sats@gmail.com>
2024-06-20 20:42:23 +0300

    STEP 013 - DONE

--- student_aleksandr_sats/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_aleksandr_sats/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,25 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    public void deleteMe() {
+    public void calculatePremiumFillingTest() {
+        Date agreementDateFrom = new Date(System.currentTimeMillis());
+        Date agreementDateTo = new Date((long) (System.currentTimeMillis() * 1.2));
+        TravelCalculatePremiumRequest request =
+                new TravelCalculatePremiumRequest("FirstName", "LastName", agreementDateFrom, agreementDateTo);
 
+        TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertThat(response).usingRecursiveComparison().isEqualTo(response);
     }
-
 }
