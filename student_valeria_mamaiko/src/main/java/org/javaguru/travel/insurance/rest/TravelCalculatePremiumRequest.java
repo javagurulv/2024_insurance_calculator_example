c29ec12c0e62d931237e1133637a8a767ff42360
Valeria <valeria.mamaiko@gmail.com>
2024-06-20 17:14:06 +0300

    STEP 010 - DONE

--- student_valeria_mamaiko/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ student_valeria_mamaiko/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -1,9 +1,17 @@
 package org.javaguru.travel.insurance.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelCalculatePremiumRequest {
-
     private String personFirstName;
     private String personLastName;
     private Date agreementDateFrom;
