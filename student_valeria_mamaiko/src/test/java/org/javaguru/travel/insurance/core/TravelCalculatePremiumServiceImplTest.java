74b7922b6a061e2fcc273032a0071c0b2e6367ae
Valeria <valeria.mamaiko@gmail.com>
2024-06-21 10:17:43 +0300

    STEP 014 - DONE

--- student_valeria_mamaiko/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_valeria_mamaiko/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -17,17 +17,61 @@ class TravelCalculatePremiumServiceImplTest {
     }
 
     @Test
+    public void calculatePremiumFirstNameTest(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        String firstName = "valeria";
+        request.setPersonLastName(firstName);
+        TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+        Assertions.assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
+    }
+
+    @Test
+    public void calculatePremiumLastNameTest(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        String lastName = "mamaiko";
+        request.setPersonLastName(lastName);
+        TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+        Assertions.assertEquals(response.getPersonLastName(), request.getPersonLastName());
+    }
+
+    @Test
+    public void calculatePremiumDateFromTest(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        Date agreementDateFrom = new Date(124, 5, 1);
+        request.setAgreementDateFrom(agreementDateFrom);
+        TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+        Assertions.assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
+    }
+
+    @Test
+    public void calculatePremiumDateToTest(){
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+        Date agreementDateTo = new Date(124, 5, 20);
+        request.setAgreementDateTo(agreementDateTo);
+        TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+        Assertions.assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
+    }
+
+    /*@Test
     public void calculatePremiumTest() {
         String lastName = "mamaiko";
         String firstName = "valeria";
-        Date agreementDateFrom = new Date(2024, 6, 1);
-        Date agreementDateTo = new Date(2024, 6, 20);
+        Date agreementDateFrom = new Date(124, 5, 1);
+        Date agreementDateTo = new Date(124, 5, 20);
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
                 firstName, lastName, agreementDateFrom, agreementDateTo);
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse(
                 firstName, lastName, agreementDateFrom, agreementDateTo);
         TravelCalculatePremiumResponse funcResponse = calculatePremiumService.calculatePremium(request);
         Assertions.assertEquals(response, funcResponse);
+    }*/
+
+    @Test
+    public void dateQuestionTest() throws InterruptedException {
+        Date agreementDateFrom = new Date();
+        /*Thread.sleep(2000);*/
+        Date agreementDateTo = new Date();
+        Assertions.assertEquals(agreementDateFrom.getTime(), agreementDateTo.getTime());
     }
 
 }
