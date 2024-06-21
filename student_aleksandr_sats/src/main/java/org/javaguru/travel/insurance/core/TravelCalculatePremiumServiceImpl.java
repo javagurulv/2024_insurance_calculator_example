8172d7a891ecfeb8c050d46e43cc73cb3cb67244
Aleksandr Sats <aleksandr.sats@gmail.com>
2024-06-21 10:22:13 +0300

    STEP 018 - DONE

--- student_aleksandr_sats/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_aleksandr_sats/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -5,7 +5,6 @@ import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -13,16 +12,21 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        BigDecimal agreementPrice = calculateAgreementPrice(request);
 
-        long diffMillis = request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime();
-        long diffDays = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS);
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
         response.setPersonFirstName(request.getPersonFirstName());
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
-        response.setAgreementPrice(new BigDecimal(diffDays));
+        response.setAgreementPrice(agreementPrice);
         return response;
+    }
+
+    private BigDecimal calculateAgreementPrice(TravelCalculatePremiumRequest request) {
+        long diffMillis = request.getAgreementDateTo().getTime() - request.getAgreementDateFrom().getTime();
+        long diffDays = TimeUnit.DAYS.convert(diffMillis, TimeUnit.MILLISECONDS);
+        return new BigDecimal(diffDays);
     }
 
 }
