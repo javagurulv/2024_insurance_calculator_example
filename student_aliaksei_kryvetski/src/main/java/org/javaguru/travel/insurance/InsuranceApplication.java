753d2e08ae1e6b936bc3485b3f45f76bbe31e5ab
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-06-23 02:49:33 +0300

    step 2 done

--- student_aliaksei_kryvetski/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
+++ student_aliaksei_kryvetski/src/main/java/org/javaguru/travel/insurance/InsuranceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InsuranceApplication {
-
+public class InsuranceApplication
+{
     public static void main(String[] args) {
         SpringApplication.run(InsuranceApplication.class, args);
     }
