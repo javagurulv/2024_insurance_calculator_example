0daff811305491880587fd4ce23411bb2cb7cdb6
Pingol Konstantin <constantineblr@yandex.ru>
2024-06-23 20:26:44 +0300

    STEP 014 - DONE

--- student_konstantin_pingol/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_konstantin_pingol/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -3,6 +3,8 @@ package org.javaguru.travel.insurance.core;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -10,19 +12,44 @@ import java.util.Date;
 class TravelCalculatePremiumServiceImplTest {
 
     private final TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+    private TravelCalculatePremiumRequest request;
+    private TravelCalculatePremiumResponse response;
 
-    @Test
-    public void testCalculatePremium() {
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
+    @BeforeEach
+    public void setup() {
+        request = new TravelCalculatePremiumRequest();
         request.setPersonFirstName("John");
         request.setPersonLastName("Ivanov");
         request.setAgreementDateFrom(new Date(1719219600000L));
         request.setAgreementDateTo(new Date(1719306000000L));
+        response = service.calculatePremium(request);
+    }
 
-        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+    @Test
+    public void testPersonalFirstName() {
         assertThat(response.getPersonFirstName()).isEqualTo(request.getPersonFirstName());
+    }
+
+    @Test
+    public void testPersonalLastName() {
         assertThat(response.getPersonLastName()).isEqualTo(request.getPersonLastName());
+    }
+
+    @Test
+    public void testAgreementDateFrom() {
         assertThat(response.getAgreementDateFrom()).isEqualTo(request.getAgreementDateFrom());
+    }
+
+    @Test
+    public void testAgreementDateTo() {
         assertThat(response.getAgreementDateTo()).isEqualTo(request.getAgreementDateTo());
+    }
+
+    @Test
+    public void testCalculatePremium() {
+        assertThat(response.getPersonFirstName()).isEqualTo("John");
+        assertThat(response.getPersonLastName()).isEqualTo("Ivanov");
+        assertThat(response.getAgreementDateFrom()).isEqualTo("2024-06-24T12:00:00.000");
+        assertThat(response.getAgreementDateTo()).isEqualTo("2024-06-25T12:00:00.000");
     }
 }
