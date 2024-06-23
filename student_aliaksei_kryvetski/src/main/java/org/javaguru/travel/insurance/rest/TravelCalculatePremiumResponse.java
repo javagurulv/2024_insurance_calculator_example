77d41c4af582502c49519973f848fb596f25b5ce
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-06-23 04:43:46 +0300

    STEP 011 DONE

--- student_aliaksei_kryvetski/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_aliaksei_kryvetski/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,9 +1,15 @@
 package org.javaguru.travel.insurance.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelCalculatePremiumResponse {
-
     private String personFirstName;
     private String personLastName;
     private Date agreementDateFrom;
