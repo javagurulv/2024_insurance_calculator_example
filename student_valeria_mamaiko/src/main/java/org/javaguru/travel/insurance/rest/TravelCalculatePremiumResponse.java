d4c5cbb0cb3a85c50687193c683c73858f67499e
Valeria <valeria.mamaiko@gmail.com>
2024-06-24 15:59:37 +0300

    STEP 016 - DONE

--- student_valeria_mamaiko/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_valeria_mamaiko/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -17,6 +18,7 @@ public class TravelCalculatePremiumResponse {
     private String personLastName;
     private Date agreementDateFrom;
     private Date agreementDateTo;
+    private BigDecimal agreementPrice;
 
     @Override
     public boolean equals(Object o) {
