35c1a4ca5e6beab83ba94b44433856d4398338a5
javagurulv <javagurulv@gmail.com>
2024-06-26 08:58:00 +0300

    fix student dir name

--- student_veranika_vishviankova/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_veranika_vishviankova/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,6 +1,9 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TravelCalculatePremiumServiceImplTest {
 
