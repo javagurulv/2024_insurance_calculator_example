49f26424d6ee73fd89f4f9026b1fc191460f8046
javagurulv <javagurulv@gmail.com>
2024-06-26 08:44:13 +0300

    fix tests

--- student_valeria_mamaiko/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
+++ student_valeria_mamaiko/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
@@ -5,23 +5,22 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 public class DateTimeServiceTest {
 
     @Test
     public void correctResult(){
-        LocalDate agreementDateFrom = LocalDate.of(2024, 6, 1);
-        LocalDate agreementDateTo = LocalDate.now();
+        LocalDate agreementDateFrom = LocalDate.of(2024, 10, 1);
+        LocalDate agreementDateTo = LocalDate.of(2024, 10, 24);
         BigDecimal result = DateTimeService.countAgreementPrice(agreementDateFrom, agreementDateTo);
         AdditionalTools.assertEquals(new BigDecimal(23), result, true);
     }
 
     @Test
     public void reverseDates(){
-        LocalDate agreementDateTo = LocalDate.of(2024, 6, 1);
-        LocalDate agreementDateFrom = LocalDate.now();
-        BigDecimal result = DateTimeService.countAgreementPrice(agreementDateFrom, agreementDateTo);
+        LocalDate agreementDateFrom = LocalDate.of(2024, 10, 1);
+        LocalDate agreementDateTo = LocalDate.of(2024, 10, 24);
+        BigDecimal result = DateTimeService.countAgreementPrice(agreementDateTo, agreementDateFrom);
         AdditionalTools.assertEquals(new BigDecimal(-23), result, true);
     }
 
