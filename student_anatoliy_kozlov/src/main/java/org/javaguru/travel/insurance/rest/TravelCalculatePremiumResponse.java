106b935330200786308188679a20df28a1a2c7a4
Kozlov Anatoly <anatoly.kozlov.ak@yandex.ru>
2024-06-29 18:08:31 +0300

    STEP 11 - DONE

--- student_anatoliy_kozlov/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_anatoliy_kozlov/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -9,4 +9,64 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
+    public TravelCalculatePremiumResponse() {}
+
+    public TravelCalculatePremiumResponse(
+            String personFirstName,
+            String personLastName,
+            Date agreementDateFrom,
+            Date agreementDateTo
+    )
+    {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.agreementDateFrom = agreementDateFrom;
+        this.agreementDateTo = agreementDateTo;
+    }
+
+    ///////
+
+    // Getters and Setters
+
+    public String GetFirstName()
+    {
+        return personFirstName;
+    }
+
+    public void SetFirstName(String FirstName)
+    {
+        this.personFirstName = FirstName;
+    }
+
+    public String GetLastName()
+    {
+        return personLastName;
+    }
+
+    public void SetLastName(String LastName)
+    {
+        this.personLastName = LastName;
+    }
+
+    public Date GetAgreementDateFrom()
+    {
+        return agreementDateFrom;
+    }
+
+    public void SetAgreementDateFrom(Date AgreementDateFrom)
+    {
+        this.agreementDateFrom = AgreementDateFrom;
+    }
+
+    public Date GetAgreementDateTo()
+    {
+        return agreementDateTo;
+    }
+
+    public void SetAgreementDateTo(Date AgreementDateTo)
+    {
+        this.agreementDateTo = AgreementDateTo;
+    }
+
+
 }
