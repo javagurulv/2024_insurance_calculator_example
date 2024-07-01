2d8f5e27854d214e6afaef4245b612e48793b6cb
Stepa0425 <karapetyanstepa@gmail.com>
2024-07-01 16:49:00 +0300

    STEP 013 - DONE

--- student_stepan_karapetyan/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_stepan_karapetyan/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,6 +1,12 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
 
@@ -9,4 +15,18 @@ class TravelCalculatePremiumServiceImplTest {
 
     }
 
+    @Test
+    public void calculatePremium() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("James",
+                                                                                "LeBron",
+                                                                                    new Date(),
+                                                                                    new Date());
+        TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(response.getPersonFirstName(), response.getPersonFirstName());
+        assertEquals(response.getPersonLastName(), response.getPersonLastName());
+        assertEquals(response.getAgreementDateFrom(), response.getAgreementDateFrom());
+        assertEquals(response.getAgreementDateTo(), response.getAgreementDateTo());
+    }
 }
